dc449fee28efdfee80607825ec8a6b0059480871
Orainge <coderhuang@outlook.com>
2022-03-31 02:15:37 +0800

    新增 HTTP 请求客户端

---
+++ spring-boot-utils/src/main/java/com/orainge/tools/spring_boot/utils/httpClient/UrlEncodeMethod.java
@@ -0,0 +1,18 @@
+package com.orainge.tools.spring_boot.utils.httpClient;
+
+/**
+ * 自定义的 URLEncode 方法
+ *
+ * @author orainge
+ * @since 2021/8/22
+ */
+public interface UrlEncodeMethod {
+    /**
+     * 使用 URLEncode 进行编码
+     *
+     * @param str 原字符串
+     * @return 编码后的字符串
+     */
+    String encode(String str);
+}
+
